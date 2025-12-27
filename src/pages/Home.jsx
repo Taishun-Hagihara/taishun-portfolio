1c67d2217b79dcaaebe0278fad9ddfe203d2f83e
Taishun <hagitaishun@gmail.com>
2025-12-27 10:51:53 +0900

    Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import ContactLink from "../components/ContactLinks";
+import Contact from "./Contact.jsx";
 
 export default function Home() {
     return (
         <div>
-            <header className="mt-15 h-100">
+            <header className="mt-15 h-150">
                 <div>
                     <h1 className="text-neutral-800 text-8xl w-10/11 mx-auto font-bold mt-30">Tai<span className="text-blue-700">shun</span></h1>
                     <h1 className="text-neutral-800 text-8xl w-10/11 mx-auto font-bold">Hagihara</h1>
@@ -12,7 +13,7 @@ export default function Home() {
                     <p>I’m a student at Doshisha University, Faculty of Life and Medical Sciences, majoring in Medical Informatics. I’m currently studying programming.</p>
                 </div>
                 <div>
-                    
+               
                 </div>
             </header>
         </div>
